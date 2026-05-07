28cd61f23de94476958ab7fa42b50d78cb524a55
Rafael Gimenes <rafaelrgimenes@gmail.com>
2026-05-06 21:01:34 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+from controllers.filme_controller import FilmeController
 
+def main():
+    controller = FilmeController()
+    print("--- CINE-SISTEMA ---")
+    filmes = controller.exibir_catalogo()
+    for f in filmes:
+        print(f"Filme: {f['titulo']} | Gênero: {f['genero']}")
+
+if __name__ == "__main__":
+    main()
